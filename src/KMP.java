ec123795f6706b467d7ac1de528819fb0c74e1ab
Jayne Gale <jaynegale@gmail.com>
2021-06-02 22:51:00 +1200

    KMP works for pi and war

--- src/KMP.java
+++ src/KMP.java
@@ -32,19 +32,14 @@ public class KMP {
      * exists, or -1 if it doesn't.
      */
     public static int search(String text, String pattern) {
-        // TODO find first occurrence of pattern S in text T
-//        pattern = "4447";
-//        The starting index of the first match in the text, or -1 if no match
+        // Goal: find first occurrence of pattern S in text T
+//        Returns the starting index of the first match in the text, or -1 if no match
 
-//        – T.indexOf(S);  indexOf() returns the position of the first occurrence of specified character(s) in a string.
-//        – T.lastIndexOf(S);
-//        – T.contains(S);
 //        KMP algorithm - first form a jump table for the pattern
         char[] S = pattern.toCharArray(); //patterm as char
         int sLength = S.length; // S
         char[] T = text.toCharArray(); //text as char
         int tLength = T.length; // T
-        System.out.println("T length " + tLength + " S length " + sLength);
 
 //        char[] partT = new char[128];
 //        System.arraycopy(T, 0, partT, 0,128);
@@ -52,17 +47,14 @@ public class KMP {
 
         //   build the jumpTable
         int[] M = createJumpTable(S);
-        System.out.println("jump table M " + Arrays.toString(M) + " for String " + Arrays.toString(S));
-//        System.out.println("T length " + tLength + " S length " + sLength);
+//        System.out.println("jump table M " + Arrays.toString(M) + " for String " + Arrays.toString(S));
 
         // KMP algorithm
         int k = 0; // start of current match in T; initially at the first character T[0]
         int i = 0; // position of current character in S, initially at S[0]
 
         while (k + i < tLength) { // can stop before text length - no need to search if the remaining string is longer than the text left to search
-//            System.out.println("T[ " + k + "] " + T[k] );
             if (S[i] == T[k + i]) {
-//                System.out.println("match at k= " + k + " T[k+1] " + T[k + 1] + " i= " + i + " S[i] " + S[i]);
                 i++;
                 if (i == sLength) { // if whole string matches! Yay
                     return k;
@@ -71,16 +63,15 @@ public class KMP {
             else if (M[i] == -1) { // the only time M[i] = -1 is when it fails on the first letter?? i = 0
                 k = k + i + 1;
                 i = 0; // mismatch with no self overlap = jumo forward the whole length of S
-//                System.out.println("mismatch at k " + k);
             }
             else {
-//                System.out.println("mismatch at k: " + k + " i " + i + " M[i] " + M[i]);
                 k = k + i - M[i]; // here is a jump
                 i = M[i];
             }
             }
         return -1;
     }
+
     public static int[] createJumpTable(char[] S) {
         int sLength = S.length; // S
         int[] M = new int[sLength]; //M
